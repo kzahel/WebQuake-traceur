ab0856772fbf31580d7eda9ca3c195dc64b59665
Kyle Graehl <kgraehl@gmail.com>
2013-09-07 02:17:05 -0700

    remove google analytics, add missing file

---
+++ background.js
@@ -0,0 +1,15 @@
+
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+    console.log('app loaunch')
+    
+    var args = "+skill 3 +map e1m1"
+    
+    chrome.app.window.create('Client/WebQuake.htm' + '?' + encodeURIComponent(args),
+                             { defaultWidth: 512,
+                               id:'WebQuake',
+                               defaultHeight: 384  },
+                             function(w) {
+                                 console.log('window created');
+                             })
+
+})
--- cpa/quake.js
+++
@@ -1,3 +0,0 @@
-var service = analytics.getService('WebQuake');
-var tracker = service.getTracker("UA-36876144-2");
-//var _gaq = _gaq || []; // just in case causes errors
